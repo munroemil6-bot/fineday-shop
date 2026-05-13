ee6c6f3fa38c8e78eec888943d1655e8e2c4f284
mylesmunroe <munroemil6@gmail.com>
2026-05-13 11:09:38 +0300

    home page created highlighting aboiut fine day

---
+++ fineday-shop/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="text-5xl font-bold mb-6">
+        Welcome To Fine Day shop
+      </h1>
+
+      <p className="text-xl text-gray-700">
+        Shop products with real-time inventory updates.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
