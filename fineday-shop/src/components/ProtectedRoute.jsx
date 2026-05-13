5aa848ef810401336fae1e88a0cf0e5f5be9a022
mylesmunroe <munroemil6@gmail.com>
2026-05-13 11:22:35 +0300

    protecetd route to secure childern shoppers

---
+++ fineday-shop/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({
+  user,
+  children,
+}) {
+  if (!user) {
+    return (
+      <Navigate to="/admin-login" />
+    );
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
